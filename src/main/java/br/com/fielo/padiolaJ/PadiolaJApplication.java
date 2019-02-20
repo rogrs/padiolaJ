f19a4966c8d3417adb17c06c8060c4932aa1bc24
rogerio ribeiro <rogerio.rs@gmail.com>
2019-02-20 01:44:49 -0300

    Import inicial

---
+++ src/main/java/br/com/fielo/padiolaJ/PadiolaJApplication.java
@@ -0,0 +1,13 @@
+package br.com.fielo.padiolaJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadiolaJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadiolaJApplication.class, args);
+	}
+
+}
