944e86cb8d95ea3c7f31af28cb8f178357aa1d14
Rogerio Ribeiro <rogerio.ribeiro1@ibm.com>
2019-02-22 14:41:09 -0300

    🔁 Implementado codigo de teste de acesso a plataforma sales force

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -Dserver.port=$PORT -jar target/padiolaJ-0.0.1-SNAPSHOT.jar
+
--- src/main/java/br/com/fielo/padiolaJ/PadiolaJApplication.java
+++ src/main/java/br/com/fielo/padiolaJ/PadiolaJApplication.java
@@ -1,13 +1,30 @@
 package br.com.fielo.padiolaJ;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
+@EnableOAuth2Sso
 public class PadiolaJApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PadiolaJApplication.class, args);
-	}
+    @Autowired
+    private Force force;
+
+    @RequestMapping("/accounts")
+    public List<Force.Account> accounts(OAuth2Authentication principal) {
+        return force.accounts(principal);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadiolaJApplication.class, args);
+    }
 
 }
