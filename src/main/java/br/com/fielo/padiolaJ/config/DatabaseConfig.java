e858517c13734e5b9077a96851e01344f5dd4ebb
rogerio ribeiro <rogerio.ribeiro@fielo.com>
2019-04-29 16:55:55 -0300

    Add datasource config

---
+++ src/main/java/br/com/fielo/padiolaJ/config/DatabaseConfig.java
@@ -0,0 +1,54 @@
+package br.com.fielo.padiolaJ.config;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class DatabaseConfig {
+
+
+	private final Logger log = LoggerFactory.getLogger(DatabaseConfig.class);
+
+    @Bean
+    @Profile("heroku")
+    public DataSource postgresDataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        log.info("Initializing PostgreSQL database: {}", databaseUrl);
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' 
+                       + dbUri.getPort() + dbUri.getPath();
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource 
+            = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
+    }
+
+}
